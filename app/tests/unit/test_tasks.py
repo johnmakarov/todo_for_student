72646109800f74e1ae61c781fa46bfad7c107c0f
John Makarov <johnmakarovqa@gmail.com>
2025-10-19 20:33:00 +0300

    add all

--- app/tests/unit/test_tasks.py
+++ app/tests/unit/test_tasks.py
@@ -12,7 +12,6 @@ from app.tasks import (
 import pytest
 
 
-@pytest.mark.skip("jsdfjjsdfsjfd")
 def test_return_none_if_id_not_found():
     result = get_task_by_id(999)
     assert result is None
@@ -26,14 +25,6 @@ def test_get_task_by_id(add_new_task):
     assert result["description"] == add_new_task["description"]
 
 
-@pytest.mark.skip("jsdfjjsdfsjfd")
-def test_if_task_completed():
-    mock_task = [{"id": 1, "title": "Test", "completed": False}]
-    result = mark_task_completed(1)
-    assert result == 1
-    assert mock_task[0]["completed"] is True
-
-
 def test_get_tasks(add_new_task):
     result = get_all_tasks()
     assert len(result) == 1
@@ -44,37 +35,30 @@ def test_add_task_without_description():
         add_task()
 
 
-def test_get_completed_task():
-    add_task("Test")
+def test_get_completed_task(add_new_task):
     mark_task_completed(1)
     result = get_completed_tasks()
-    print(result)
     assert len(result) == 1
 
 
-def test_get_pending_task():
-    add_task("Test")
+def test_get_pending_task(add_new_task):
     mark_task_completed(1)
     result = get_pending_tasks()
-    print(result)
-    assert len(result) == 1
+    assert len(result) == 0
 
 
-def test_clear_all_tasks():
-    add_task("Test")
+def test_clear_all_tasks(add_new_task):
     clear_all_tasks()
     result = get_all_tasks()
     assert len(result) == 0
 
 
-def test_get_tasks_count():
-    add_task("Test")
+def test_get_tasks_count(add_new_task):
     result = get_tasks_count()
     assert result == 1
 
 
-def test_update_task_description():
-    add_task("Test")
-    update_task_description(1, "Test 2")
+def test_update_task_description(add_new_task):
+    update_task_description(task_id=1, new_description="Test 2")
     result = get_task_by_id(1)
     assert result["description"] == "Test 2"
