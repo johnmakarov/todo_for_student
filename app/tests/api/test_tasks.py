e4e740238524dafe2d36a21f1757153b8f274454
John Makarov <johnmakarovqa@gmail.com>
2025-10-19 21:14:42 +0300

    add all

--- app/tests/api/test_tasks.py
+++ app/tests/api/test_tasks.py
@@ -1,11 +1,16 @@
-import requests
+from .utils import api_request
+from http import HTTPMethod
 
 
 def test_create_task():
-    requests.delete("http://127.0.0.1:8000/tasks/")
+    # requests.delete("http://127.0.0.1:8000/tasks/")
+    api_request(method=HTTPMethod.DELETE, endpoint="/tasks/", expected_status=200)
 
-    created_task = requests.post(
-        "http://127.0.0.1:8000/tasks/", json={"description": "string"}
+    # created_task = requests.post(
+    #     url="http://127.0.0.1:8000/tasks/", json={"description": "string"}
+    # )
+
+    created_task = api_request(
+        method=HTTPMethod.POST, endpoint="/tasks", json={"description": "string"}
     )
-    assert created_task.status_code == 201
-    assert created_task.json() == {"id": 1, "description": "string", "completed": False}
+    assert created_task == {"id": 1, "description": "string", "completed": False}
