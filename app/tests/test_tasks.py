89bfc0b9a45cec2c717bff59b9be406f95254f38
Ivanna Melnichenko <melnichenko.ivanna@gmail.com>
2025-10-18 19:19:08 -0500

    Added more new tests

--- app/tests/test_tasks.py
+++ app/tests/test_tasks.py
@@ -2,11 +2,14 @@ from app.tasks import (
     get_task_by_id,
     mark_task_completed,
     get_all_tasks,
-    add_task
-
+    add_task,
+    get_completed_tasks,
+    get_pending_tasks,
+    clear_all_tasks,
+    get_tasks_count,
+    update_task_description
 )
 import pytest
-
 
 
 @pytest.mark.skip("jsdfjjsdfsjfd")
@@ -30,6 +33,7 @@ def test_if_task_completed():
     assert result == 1
     assert mock_task[0]["completed"] is True
 
+
 def test_get_tasks(add_new_task):
     result = get_all_tasks()
     assert len(result) == 1
@@ -38,4 +42,33 @@ def test_add_task_without_description():
     with pytest.raises(TypeError, match=r"missing .* required positional argument"):
         add_task()
 
+def test_get_completed_task():
+    add_task("Test")
+    mark_task_completed(1)
+    result = get_completed_tasks()
+    print(result)
+    assert len(result) == 1
 
+def test_get_pending_task():
+    add_task("Test")
+    mark_task_completed(1)
+    result = get_pending_tasks()
+    print(result)
+    assert len(result) == 1
+
+def test_clear_all_tasks():
+    add_task("Test")
+    clear_all_tasks()
+    result = get_all_tasks()
+    assert len(result) == 0
+
+def test_get_tasks_count():
+    add_task("Test")
+    result = get_tasks_count()
+    assert result == 1
+
+def test_update_task_description():
+    add_task("Test")
+    update_task_description(1, "Test 2")
+    result = get_task_by_id(1)
+    assert result["description"] == "Test 2"
